9e5157eabe1eb41dc185fb2888269d72720d8f2f
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-01-28 18:38:44 -0500

    Update app.py
    start to import flask and set up base for app py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-# app.py 
+# Import dependencies
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Database Setup
+engine = create_engine("sqlite:///......sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+
+
+# Flask Setup
+app = Flask(__name__)
+
+
+# Flask Routes
+@app.route("/")
+def 
