2cf41f6fbb18902456b47a082925d9a51cb2309d
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-01 21:01:47 -0500

    test commit

--- app.py
+++ app.py
@@ -35,56 +35,56 @@ def home_page():
             f"............."
     )
 
-# Json representation of dictionary
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# # Json representation of dictionary
+# @app.route("/api/v1.0/precipitation")
+# def precipitation():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
     
-    # Query all 
-    results = session.query().all()
+#     # Query all 
+#     results = session.query().all()
 
-    session.close()
-    return jsonify()
+#     session.close()
+#     return jsonify()
 
-#Convert the query results to a dictionary using date as the key and prcp as the value.
-#Return the JSON representation of your dictionary.
-
-
-
-# Station list
-@app.route("/api/v1.0/stations")
-def stations():
-
-    # Return a JSON list of stations from the dataset.
-    session.close()
-    return()
-
-
-# Temperature observations
-@app.route("/api/v1.0/tobs")
-def tobs():
-
-
-#Query the dates and temperature observations of the most active station for 
-#       the last year of data.
-#Return a JSON list of temperature observations (TOBS) for the previous year.
+# #Convert the query results to a dictionary using date as the key and prcp as the value.
+# #Return the JSON representation of your dictionary.
 
 
 
-# Temperature of certain dates
-@app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/<start>/<end>")
+# # Station list
+# @app.route("/api/v1.0/stations")
+# def stations():
 
-def temp_range(start, end = None):
+#     # Return a JSON list of stations from the dataset.
+#     session.close()
+#     return()
 
 
-# Return a JSON list of the minimum temperature, the average temperature, and \
-#       the max temperature for a given start or start-end range.
-# When given the start only, calculate TMIN, TAVG, and 
-#       TMAX for all dates greater than and equal to the start date.
-# When given the start and the end date, calculate the TMIN, TAVG, and TMAX 
-#       for dates between the start and end date inclusive.
+# # Temperature observations
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+
+
+# #Query the dates and temperature observations of the most active station for 
+# #       the last year of data.
+# #Return a JSON list of temperature observations (TOBS) for the previous year.
+
+
+
+# # Temperature of certain dates
+# @app.route("/api/v1.0/<start>")
+# @app.route("/api/v1.0/<start>/<end>")
+
+# def temp_range(start, end = None):
+
+
+# # Return a JSON list of the minimum temperature, the average temperature, and \
+# #       the max temperature for a given start or start-end range.
+# # When given the start only, calculate TMIN, TAVG, and 
+# #       TMAX for all dates greater than and equal to the start date.
+# # When given the start and the end date, calculate the TMIN, TAVG, and TMAX 
+# #       for dates between the start and end date inclusive.
 
 
 
